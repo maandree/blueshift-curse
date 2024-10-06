3dd66905e87fdf00632a200911907edab023e812
Mattias Andrée <m@maandree.se>
2024-10-06 16:02:34 +0200

    Update e-mail
    Signed-off-by: Mattias Andrée <m@maandree.se>

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''
 blueshift-curse – Blueshift extension with IPC and an ncurses front-end
-Copyright © 2014  Mattias Andrée (maandree@member.fsf.org)
+Copyright © 2014  Mattias Andrée (m@maandree.se)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
